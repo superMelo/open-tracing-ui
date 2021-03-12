7cbe0e6ff95a8c48f6c0ed0a2d481790cf51115d
superMelo <774876970@qq.com>
2021-03-12 09:31:36 +0800

    start

---
+++ src/main/java/com/qyf/opentracingui/OpenTracingUiApplication.java
@@ -0,0 +1,13 @@
+package com.qyf.opentracingui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTracingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTracingUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qyf/opentracingui/OpenTracingUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qyf.opentracingui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTracingUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
